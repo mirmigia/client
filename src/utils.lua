fae0af157ea817e164a6dbb024eb8afe02871243
Antonis Kalou <kalouantonis@gmail.com>
2017-03-19 17:46:48 +0200

    Detect cyclical dependencies in printTable

--- src/utils.lua
+++ src/utils.lua
@@ -1,9 +1,12 @@
+-- The number of cycles before a table reference is considered cyclical
+local N_TOO_MANY_CYCLES = 100
+
 -- Returns a string representation for value `v` for use when displaying
 -- to the user the correct representation for a value. E.g. a string
 -- should be displayed as `"Some string"` and not `Some String`
 local function toRepresentation (v)
   if type(v) == 'string' then
-    return '"' .. v .. '"'
+    return string.format('%q', v)
   else
     return tostring(v)
   end
@@ -11,8 +14,15 @@ end
 
 -- Print a human-readable representation of table `t` `level`
 -- represents the current indentation level, defaults to 0.
+--
+-- Throws an error if a cyclical reference is detected.
 function printTable (t, level)
   level = level or 1
+
+  if level > N_TOO_MANY_CYCLES then
+    error([[table contains too many nested levels, possibly indicating
+      a cyclical reference]])
+  end
 
   io.write("{\n")
   for k, v in pairs(t) do
