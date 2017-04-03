df11e6a8e89040134904924a645d63838b92f358
Antonis Kalou <kalouantonis@gmail.com>
2017-04-03 21:46:57 +0300

    Removed google fonts

--- src/mirmigia/core.cljs
+++ src/mirmigia/core.cljs
@@ -35,8 +35,7 @@
   "Canvas used for rendering the main game. `w` and `h` represent
 the width and height of the canvas element respectively."
   [w h]
-  [:canvas {:width w :height h}])
-
+  [:canvas {:width w :height h}]) 
 (rum/defc ship-view [ships]
   [:table
    [:thead
